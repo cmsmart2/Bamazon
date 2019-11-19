46a4ec0840a592a18e3ee3239c753927727b7d27
Cera Smart <cera_smart@yahoo.com>
2019-11-18 20:48:53 -0600

    added seeds and schema

---
+++ bamazonSchema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS bamazon_DB;
+CREATE DATABASE bamazon_DB;
+
+USE bamazon_DB;
+
+CREATE TABLE inventory(
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(50) NOT NULL,
+  price DECIMAL(10,2),
+  stock_quantity INT default 0,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO inventory (product_name, department_name, price)
+VALUES ("Ipod", 250, "apple");
---
+++ bamazonSeeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO inventory (product_name, department_name, price, stock_quantity)
+VALUES ("Christmas Tree", "Holiday", 200, 10);
+INSERT INTO inventory (product_name, department_name, price, stock_quantity)
+VALUES ("Christmas Ornaments", "Holiday", 20, 15);
+INSERT INTO inventory (product_name, department_name, price, stock_quantity)
+VALUES ("Ipod", "Electronics", 400, 3);
+INSERT INTO inventory (product_name, department_name, price, stock_quantity)
+VALUES ("Nintendo Switch", "Electronics", 400, 6);
+INSERT INTO inventory (product_name, department_name, price, stock_quantity)
+VALUES ("Cutting Board", "Kitchen", 30, 12);
+INSERT INTO inventory (product_name, department_name, price, stock_quantity)
+VALUES ("Mixing Bowl", "Kitchen", 15, 17);
+INSERT INTO inventory (product_name, department_name, price, stock_quantity)
+VALUES ("Picture Frame", "Home Decoration", 35, 5);
+INSERT INTO inventory (product_name, department_name, price, stock_quantity)
+VALUES ("Shelf", "Home Decoration", 25, 8);
+INSERT INTO inventory (product_name, department_name, price, stock_quantity)
+VALUES ("Candle", "Misc", 10, 40);
+INSERT INTO inventory (product_name, department_name, price, stock_quantity)
+VALUES ("Chapstick", "Misc", 2, 15);
